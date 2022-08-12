7d5c3e8f6adc8ee10b8679a7b09902b4721611b1
Tophatsynth <timsam7453@gmail.com>
2022-08-12 10:20:51 +0800

    Added exception handling for invalid values in player choices also cleaned up the code so it looks a bit tidier

--- tictactoe.py
+++ tictactoe.py
@@ -6,12 +6,25 @@ class TicTacToe():
     def __init__(self) -> None:
         self.board = []
 
+    # Creating the board
+
     def create_board(self) -> None:
         for i in range(3):
             row = []
             for j in range(3):
                 row.append("-")
             self.board.append(row)
+
+    # Player turns
+
+    def choose_random_player(self) -> None:
+        self.player = random.choice(["X", "O"])
+        print(f"Player {self.player} goes first! \n")
+    
+    def swap_turns(self):
+        self.player = "X" if self.player == "O" else "O"
+
+    # Display functions 
     
     def display_board(self) -> None:
         for row in self.board:
@@ -19,77 +32,10 @@ class TicTacToe():
                 print(item, end=" ")
             print()
 
-    def grab_player_choice(self) -> list:
-        return list(map(int, input(f"Player {self.player}'s turn: ").split()))
-        
-        
-    def save_player_choice(self, choices:list) -> None:
-        row, col = choices
-        self.board[row-1][col-1] = self.player
-            
-
-    def check_for_win(self) -> bool:
-        win = True
-
-        for i in range(3):
-            win = True
-            for j in range(3):
-                if self.board[i][j] != self.player:
-                    win = False
-                    break
-
-            if win: 
-                return win
-        
-        win = True
-        
-        for i in range(3):
-            win = True
-            for j in range(3):
-                if self.board[j][i] != self.player:
-                    win = False
-                    break
-            
-            if win:
-                return win
-            
-        win = True
-
-        for i in range(3):
-            if self.board[i][i] != self.player:
-                win = False
-
-        if win:
-            return win
-        
-        win = True
-
-        for i in range(3):
-            if self.board[i][3 - 1 - i] != self.player:
-                win = False
-                break
-        
-        if win:
-            return win
-            
-        return False
-
-    def check_filled(self) -> bool:
-        for row in self.board:
-            for item in row:
-                if item == "-":
-                    return False
-        return True
-        
-    def choose_random_player(self) -> None:
-        self.player = random.choice(["X", "O"])
-        print(f"Player {self.player} goes first! \n")
-    
-    def swap_turns(self):
-        self.player = "X" if self.player == "O" else "O"
-    
     def display_intro(self) -> None:
-        print("""Tic Tac Toe Game
+        print("""- - - - - - -
+
+Tic Tac Toe Game
         
 Each player chooses X or O
         
@@ -105,6 +51,8 @@ This will display:
 (row 1, column 3)
         
 We will start the game now. 
+
+- - - - - - -
 """)
     
     def display_win(self) -> None:
@@ -112,18 +60,93 @@ We will start the game now.
 
     def display_draw(self) -> None:
         print("It's a draw! \n")
-
-    def restart_confirmation(self) -> bool:
-        return True if input("Would you like to restart? (y/n)") == "y" else False
     
     def display_end(self) -> None:
-        print("Thank you for playing! :D")
+        print("\nThank you for playing! :D \n")
+
+    # Player choice inputs
+
+    def grab_player_choice(self) -> list:
+        try: 
+            return list(map(int, input(f"Player {self.player}'s turn: ").split()))
+        except ValueError:
+            return False
+        
+    def save_player_choice(self, choices:list) -> None:
+        row, col = choices
+        self.board[row-1][col-1] = self.player
+
+    def is_player_choice_valid(self, choices:list) -> list:
+        try:
+            row, col = choices
+            return [True] if self.board[row-1][col-1] == "-" else [False, "That space is already filled! Try again \n"]
+        except ValueError:
+            return [False, "Missing row or column! Try again \n"] if len(choices) < 2 else [False, "Too many values! Try again \n"]
+        except TypeError:
+            return [False, "That's not a number! Try again \n"]            
+        except IndexError:
+            return [False, "That's not within the board! Try again \n"]
+    
+    def player_choice_not_valid(self, error_msg:list) -> None:
+        print(error_msg[1])
+            
+    # Checking the board
+
+    def check_for_win(self) -> bool:
+        win = True
+
+        for i in range(3):
+            win = True
+            for j in range(3):
+                if self.board[i][j] != self.player:
+                    win = False
+                    break
+            if win: 
+                return win
+        win = True
+        
+        for i in range(3):
+            win = True
+            for j in range(3):
+                if self.board[j][i] != self.player:
+                    win = False
+                    break
+            if win:
+                return win
+        win = True
+
+        for i in range(3):
+            if self.board[i][i] != self.player:
+                win = False
+        if win:
+            return win
+        win = True
+
+        for i in range(3):
+            if self.board[i][3 - 1 - i] != self.player:
+                win = False
+                break
+        if win:
+            return win
+        return False
+
+    def check_filled(self) -> bool:
+        for row in self.board:
+            for item in row:
+                if item == "-":
+                    return False
+        return True
+
+    # Check to restart the game    
+
+    def restart_confirmation(self) -> bool:
+        return True if input("Would you like to restart? (y/n): ") == "y" else False
+    
+    # Main function
 
     def start_game(self) -> None:
         while True:
             self.display_intro()
-
-            sleep(1)
 
             self.create_board()
             self.choose_random_player()
@@ -132,7 +155,12 @@ We will start the game now.
                 self.display_board()
                 choices = self.grab_player_choice()
                 print()
-                self.save_player_choice(choices)
+                error = self.is_player_choice_valid(choices)
+                if error[0]:
+                    self.save_player_choice(choices)
+                else:
+                    self.player_choice_not_valid(error)
+                    continue
 
                 if self.check_for_win():
                     self.display_win()
